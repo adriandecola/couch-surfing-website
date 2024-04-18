f2e8f53a040a63deee82bfe1fafff15c914202cc
Adrian deCola <adriandecola@gmail.com>
2024-04-17 21:40:43 -0400

    Added Popertiy, review, and welcome functionality, all useing typescript. Also added a build.sh file to compile the typescript and rename imports in compiled js files so this project can be viewed directly in the browser.

--- index.ts
+++ index.ts
@@ -1,49 +1,182 @@
-const reviewTotalDisplay = document.querySelector('#reviews')
-const returningUserDisplay = document.querySelector('#returning-user')
-const userNameDisplay = document.querySelector('#user')
+// Wrapping up our Dashboard
+// 1. Create All the other interfaces you think are needed for this board
+// 2. Using the Class, visually show the main Image above the review button.
+import {
+	showReviewTotal,
+	populateUser,
+	showDetails,
+	getTopTwoReviews,
+} from './utils';
+import { Price, Country } from './types';
+import { Permissions, LoyaltyUser } from './enums';
+import Review from './interfaces';
+const propertyContainer = document.querySelector('.properties');
+const reviewContainer = document.querySelector('.reviews');
+const container = document.querySelector('.container');
+const button = document.querySelector('button');
+const footer = document.querySelector('.footer');
 
-const reviews = [
+let isLoggedIn: boolean;
+
+// Reviews
+const reviews: Review[] = [
     {
-        name: 'Sheia',
+        name: 'Sheila',
         stars: 5,
-        loyaltyUser: true,
-        date: '01-04-2021'
+        loyaltyUser: LoyaltyUser.GOLD_USER,
+        date: '01-04-2021',
     },
     {
         name: 'Andrzej',
         stars: 3,
-        loyaltyUser: false,
-        date: '28-03-2021'
+        loyaltyUser: LoyaltyUser.BRONZE_USER,
+        date: '28-03-2021',
     },
     {
         name: 'Omar',
         stars: 4,
-        loyaltyUser: true,
-        date: '27-03-2021'
+        loyaltyUser: LoyaltyUser.SILVER_USER,
+        date: '27-03-2021',
     },
-]
-
-// Function do display total number of reviews
-function showReviewTotal (reviewTotal: number, lastReviewer: string, loyaltyUser: boolean) {
-	if (reviewTotalDisplay) {
-        reviewTotalDisplay.innerHTML = "Total Number of Reviews: " + reviewTotal.toString() + " | Last Reviewer: " + lastReviewer + (loyaltyUser ? '⭐' : '');    
-    }
-}
-
-showReviewTotal(reviews.length, "Andrzej", true);
+];
 
 const you = {
-    userName: 'Bobby',
+    firstName: 'Bobby',
+lastName: 'Brown',
+	permissions: Permissions.ADMIN,
     isReturning: true,
+age: 35,
+	stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow'],
+};
+
+interface Property {
+	image: string;
+	title: string;
+	price: Price;
+	location: {
+		firstLine: string;
+		city: string;
+		code: number | string;
+		country: Country;
+	};
+	contact: [number, string];
+	isAvailable: boolean;
 }
 
+// Array of Properties
+const properties: Property[] = [
+	{
+		image: 'images/colombia-property.jpg',
+		title: 'Colombian Shack',
+		price: 45,
+		location: {
+			firstLine: 'shack 37',
+			city: 'Bogota',
+			code: 45632,
+			country: 'Colombia',
+		},
+		contact: [+112343823978921, 'marywinkle@gmail.com'],
+		isAvailable: true,
+	},
+	{
+		image: 'images/poland-property.jpg',
+		title: 'Polish Cottage',
+		price: 30,
+		location: {
+			firstLine: 'no 23',
+			city: 'Gdansk',
+			code: 343903,
+			country: 'Poland',
+		},
+		contact: [+1298239028490830, 'garydavis@hotmail.com'],
+		isAvailable: false,
+	},
+	{
+		image: 'images/london-property.jpg',
+		title: 'London Flat',
+		price: 25,
+		location: {
+			firstLine: 'flat 15',
+			city: 'London',
+			code: 'SW4 5XW',
+			country: 'United Kingdom',
+		},
+		contact: [+34829374892553, 'andyluger@aol.com'],
+		isAvailable: true,
+	},
+];
 
-function populateUser(isReturning : boolean, userName : string ) {
-    if (isReturning && returningUserDisplay){
-        returningUserDisplay.innerHTML = 'back'
-    }
-    if (userNameDisplay) userNameDisplay.innerHTML = userName;
-    
+// Functions
+showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
+
+populateUser(you.isReturning, you.firstName);
+
+// Add the properties
+for (let i = 0; i < properties.length; i++) {
+	const card = document.createElement('div');
+	card.classList.add('card');
+	card.innerHTML = properties[i].title;
+	const image = document.createElement('img');
+	image.setAttribute('src', properties[i].image);
+	card.appendChild(image);
+	showDetails(you.permissions, card, properties[i].price);
+	propertyContainer && propertyContainer.appendChild(card);
 }
 
-populateUser(you.isReturning, you.userName)
+let count = 0;
+function addReviews(array: Review[]): void {
+	if (!count) {
+		count++;
+		const topTwo = getTopTwoReviews(array);
+		for (let i = 0; i < topTwo.length; i++) {
+			const card = document.createElement('div');
+			card.classList.add('review-card');
+			card.innerHTML = topTwo[i].stars + ' stars from ' + topTwo[i].name;
+			reviewContainer && reviewContainer.appendChild(card);
+		}
+		container && button && container.removeChild(button);
+	}
+}
+
+button && button.addEventListener('click', () => addReviews(reviews));
+
+let currentLocation: [string, string, number] = ['London', '11.03', 17];
+if (footer) {
+	footer.innerHTML =
+		currentLocation[0] +
+		' ' +
+		currentLocation[1] +
+		' ' +
+		currentLocation[2] +
+		'°';
+}
+
+// Classes
+class MainProperty {
+	src: string;
+	title: string;
+	reviews: Review[];
+	constructor(src: string, title: string, reviews: Review[]) {
+		this.src = src;
+		this.title = title;
+		this.reviews = reviews;
+	}
+}
+
+let yourMainProperty = new MainProperty(
+	'images/italian-property.jpg',
+	'Italian House',
+	[
+		{
+			name: 'Olive',
+			stars: 5,
+			loyaltyUser: LoyaltyUser.GOLD_USER,
+			date: '12-04-2021',
+		},
+	]
+);
+
+const mainImageContainer = document.querySelector('.main-image');
+const image = document.createElement('img');
+image.setAttribute('src', yourMainProperty.src);
+mainImageContainer && mainImageContainer.appendChild(image);
